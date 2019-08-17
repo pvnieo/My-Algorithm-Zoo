6f5da4b9b0422f3a2dabbbe3292f0cfaa0370258
pvnieo <22404728+pvnieo@users.noreply.github.com>
2019-08-18 00:08:28 +0200

    add doc

--- Learning rate finder/lr_finder.py
+++ Learning rate finder/lr_finder.py
@@ -7,7 +7,20 @@ import torch
 
 
 class LRFinder:
+    """Implements Leslie N. Smith's Learning rate finder method. Use `.fin()` method to search for the best learning rate.
+       use `.plot()` method to plot the loss
+    """
+
     def __init__(self, model, optimizer, criterion, cuda=True):
+        """
+        Arguments:
+            model {torch.nn.Module} -- Used model
+            optimizer {torch.optim.Optimizer} -- Used optimizer
+            criterion {torch.nn.Module)} -- Loss function
+
+        Keyword Arguments:
+            cuda {bool} -- Use cuda if available (default: {True})
+        """
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
@@ -19,6 +32,19 @@ class LRFinder:
         self.criterion.to(self.device)
 
     def find(self, train_loader, val_loader=None, num_iter=100, init_value=1e-6, final_value=10., div_th=5, beta=0.98):
+        """Performs the learning rate range test.
+
+        Arguments:
+            train_loader {torch.utils.data.DataLoader} -- Training set data loader
+
+        Keyword Arguments:
+            val_loader {torch.utils.data.DataLoader} -- Validation set dataloader. If None, range test will be performed only with train_loader (default: {None})
+            num_iter {int} -- Maximum number of iteration. Determines the discretisation of the interval (default: {100})
+            init_value {float} -- Minimun learning rate to start with. (default: {1e-6})
+            final_value {float} -- Maximum learnig rate before stopping the range test (default: {10.})
+            div_th {int} -- Stop the range test if the loss attains div_th * min_loss (default: {5})
+            beta {float} -- Parameter used to smooth the loss. must be in [0, 1) (default: {0.98})
+        """
         best_loss = float("inf")
         lr = init_value
         self.optimizer.param_groups[0]['lr'] = lr
@@ -58,6 +84,19 @@ class LRFinder:
         print("LR Finder is complete. See the graph using `.plot()` method.")
 
     def _train_val_model(self, inputs, labels, val_loader, phase="train"):
+        """train the model for one mini-batch if phase==train, perform a validation step otherwise
+
+        Arguments:
+            inputs {torch.Tensor} -- Input data
+            labels {torch.Tensor} -- Labels of the input data
+            val_loader {torch.utils.data.DataLoader} -- Validation set dataloader.
+
+        Keyword Arguments:
+            phase {str} -- Either `train` or `val` (default: {"train"})
+
+        Returns:
+            {float} -- loss obtained
+        """
 
         if phase == 'train':
             self.model.train()  # Set model to training mode
@@ -94,6 +133,15 @@ class LRFinder:
         return epoch_loss
 
     def plot(self, skip_start=10, skip_end=5, log_lr=True, smooth=True, save_plot_to=None):
+        """Plot the learning rate range test.
+
+        Keyword Arguments:
+            skip_start {int} -- number of batches to trim from the start (default: {10})
+            skip_end {int} -- number of batches to trim from the end (default: {5})
+            log_lr {bool} -- True to plot the learning rate in a logarithmic scale (default: {True})
+            smooth {bool} -- True to smooth the loss function using UnivariateSpline smoother (default: {True})
+            save_plot_to {[type]} -- Path to where to save the figure. None to disable saving. (default: {None})
+        """
         assert (skip_start >= 0 and skip_end >= 0), "skip_start and skip_end must be>=0!"
 
         lrs = self.history["lr"][skip_start:-skip_end] if skip_end > 0 else self.history["lr"][skip_start:]
